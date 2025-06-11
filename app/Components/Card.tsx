3c89ec6a64e42e403079c896ee4faad8684213fc
Soham <sohamsah28@gmail.com>
2025-06-11 16:42:39 -0700

    fixed card

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -16,7 +16,7 @@ const Skills: React.FC<{ skills: string[] }> = ({ skills }) => {
       {skills.map((skill, index) => (
         <a
           key={index}
-          className="px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          className="px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           style={{ marginBottom: '10px' }}
         >
           {skill}
@@ -30,17 +30,17 @@ const Card: React.FC<CardProps> = ({ title, description, skills, btnTxt, link })
   return (
     <div
       data-theme={Theme}
-      className="p-6 w-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition-transform hover:scale-105 hover:rotate-2"
+      className="p-6 w-96 rounded-lg shadow bg-gray-800 border-gray-700 transition-transform hover:scale-105 hover:rotate-2"
       style={{ marginTop: '15px', marginRight: '20px' }}
     >
       <div className="flex flex-col h-full justify-between">
         <div>
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-success">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white hover:text-success">
               {title}
             </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+          <p className="mb-3 font-normal text-gray-400">{description}</p>
           <Skills skills={skills} />
         </div>
         <a href={link}>
